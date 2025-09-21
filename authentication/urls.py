fd5e6bb16ed36fffda0e680049fd0e48aa26c00a
ARUNIL KESHRI <arunilkeshri03@gmail.com>
2025-09-21 17:28:10 +0530

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import UserCreateAPIView, TokenObtainPairView
 
+urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
