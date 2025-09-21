a107abfc8728f37c948e9ab513f8ae6a12a6f19d
ARUNIL KESHRI <arunilkeshri03@gmail.com>
2025-09-21 17:26:42 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,1 +1,15 @@
+from django.db import models
 
+class Case(models.Model):
+    case_number = models.CharField(max_length=255, unique=True)
+    defendant_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    crime_type = models.CharField(max_length=255)
+    prior_convictions = models.IntegerField()
+    flight_risk_score = models.FloatField(null=True, blank=True)
+    recidivism_score = models.FloatField(null=True, blank=True)
+    bail_recommendation = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Case: {self.case_number} - {self.defendant_name}"
