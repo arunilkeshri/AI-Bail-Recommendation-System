4001f076845af082692e952294ca26405a040780
ARUNIL KESHRI <arunilkeshri03@gmail.com>
2025-09-21 17:28:03 +0530

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,1 +1,10 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UserSerializer
 
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
